965a07f77d5d9f195abccb54f6eb5a30a43b50ea
Alex <alex.lipinsky@gmail.com>
2022-10-02 11:50:57 -0400

    datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input'
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
 })
-
 export class MaterialModule {}
