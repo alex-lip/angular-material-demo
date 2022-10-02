c0f91f3c6f3ec55aec5af5cadbedb6718d9ea745
Alex <alex.lipinsky@gmail.com>
2022-10-02 09:36:22 -0400

    new components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [MatButtonModule, MatIconModule],
+    exports: [MatButtonModule, MatIconModule]
 })
 
 export class MaterialModule {}
