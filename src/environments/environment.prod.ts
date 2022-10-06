625243098faaa4917a44f2f23065685839252ec7
Alex <alex.lipinsky@gmail.com>
2022-10-06 19:56:15 -0400

    connected firebase with angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'ng-fitness-tracker-4a518',
+    appId: '1:801828496935:web:d7b0b6a9d675e4a3441002',
+    storageBucket: 'ng-fitness-tracker-4a518.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCSloxE0Q0yYE0aGLgPn2OI1xC8TfeqLo0',
+    authDomain: 'ng-fitness-tracker-4a518.firebaseapp.com',
+    messagingSenderId: '801828496935',
+    measurementId: 'G-LMR813VV72',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ng-fitness-tracker-4a518',
+    appId: '1:801828496935:web:d7b0b6a9d675e4a3441002',
+    storageBucket: 'ng-fitness-tracker-4a518.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCSloxE0Q0yYE0aGLgPn2OI1xC8TfeqLo0',
+    authDomain: 'ng-fitness-tracker-4a518.firebaseapp.com',
+    messagingSenderId: '801828496935',
+    measurementId: 'G-LMR813VV72',
+  },
   production: false
 };
 
